6fe926fd23ebb17b6692204610e1702b9073cbb6
Carl <carljarms@gmail.com>
2020-03-19 18:43:10 -0500

    Initial commit

---
+++ src/main/java/com/scififics/scifificswebsite/ScifificsWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.scififics.scifificswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScifificsWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScifificsWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scififics/scifificswebsite/ScifificsWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scififics.scifificswebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScifificsWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
