2fb2b0ec3664d65ee058a87da1ad257db3707b55
Carl <carljarms@gmail.com>
2020-04-03 15:41:56 -0500

    Set up database

---
+++ src/main/java/com/scififics/scifificswebsite/models/Story.java
@@ -0,0 +1,18 @@
+package com.scififics.scifificswebsite.models;
+
+public class Story {
+
+    private String title;
+
+    public Story(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
